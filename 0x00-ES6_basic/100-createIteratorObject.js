d84b990a4e32981f7230b28f3787b7bb4d82499e
Stephen Obialor <stevobialor@gmail.com>
2023-04-18 04:43:23 +0000

    done

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject(report) {
+  const myReport = [];
+  for (const depart of object.values(report.allEmployees)) {
+      myReport.push(...emp);
+  }
+  return myReport;
+}
