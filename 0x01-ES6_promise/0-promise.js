8f890c57cd1b443b1754f8c3170396eaab5d9b5f
Stephen Obialor <stevobialor@gmail.com>
2023-04-19 20:50:03 +0000

    done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    fetch('https://github.com/api/data')
-      .then(response => {
-        if (response.ok) {
-          resolve(response.json());
-        } else {
-          reject(new Error(`API call failed with status ${response.status}`));
-        }
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
+  return new Promise(() => {});
 }
-
