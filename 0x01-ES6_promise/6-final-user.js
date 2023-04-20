2169509d72e8677672725f090b84ac05d52dc0db
Stephen Obialor <stevobialor@gmail.com>
2023-04-20 04:39:01 +0100

    make code more concise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((r) => {
-      const arr = [];
-      r.map((ob) => (
-        arr.push({
-          status: ob.status,
-          value: ob.status === 'fulfilled' ? ob.value : String(ob.reason)
-        })
-      ));
-      return arr;
-    });
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((r) => (
+      r.map((ob) => ({
+        status: ob.status,
+        value: ob.status === 'fulfilled' ? ob.value : String(ob.reason),
+      }))
+    ));
 }
