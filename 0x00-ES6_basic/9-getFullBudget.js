e1639fce39e374572fa5937cafa7df04c19333fe
Stephen Obialor <stevobialor@gmail.com>
2023-04-23 22:20:00 +0100

    done

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,12 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    const getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    const getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
+    getIncomeInDollars(income) => `$${income}`,
+    getIncomeInEuros(income) => `${income} euros`
   };
 
   return fullBudget;
