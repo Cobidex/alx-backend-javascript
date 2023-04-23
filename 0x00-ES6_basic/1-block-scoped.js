01a13e3aec57e7fe087ebbb315090d41bbdb06e0
Stephen Obialor <stevobialor@gmail.com>
2023-04-23 21:39:40 +0100

    make more concise

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,8 @@
-export default function taskBlock(val) {
+export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
-  if (val) {
+  if (trueOrFalse) {
     let task = true;
     let task2 = false;
   }
