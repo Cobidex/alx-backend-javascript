4107bbc976f4fed866b1dcb37b53fdb535fdb948
Stephen Obialor <stevobialor@gmail.com>
2023-04-25 19:16:35 +0000

    done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!startString || !startString.length) return '';
 
   const array = [...set]
-    .filter((string) => string.startsWith(startString))
+    .filter((string) => if (string && string.startsWith(startString)) return string)
     .map((string) => string.slice(startString.length));
   return array.join('-');
 }
