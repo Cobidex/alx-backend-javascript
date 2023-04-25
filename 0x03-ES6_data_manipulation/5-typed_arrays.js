6ec750691d64cd5ce12111f37d56fb0db876c1b9
Stephen Obialor <stevobialor@gmail.com>
2023-04-25 17:06:39 +0000

    make code more concise and readable

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,8 @@
 export default createInt8TypedArray(length, position, value) {
   const array = new ArrayBuffer(length);
-  const view = new DataView(array);
+  const view = new DataView(array, 0);
 
-  if (position >= length) {
-    throw new Error('Position outside range');
-  }
+  if (position >= length) throw new Error('Position outside range');
 
   view.setInt8(position, value);
 
