64fbd19b67a35070d957f1fa4171a2dc21c35605
Stephen Obialor <stevobialor@gmail.com>
2023-04-25 15:33:39 +0000

    return grades

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeOb = newGrades
+        .filter((gradeOb) => student.id === gradeOb.studentId)
+        .map((gradeOb) => gradeOb.grade)[0];
+      const grade = gradeOb ? gradeOb.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
