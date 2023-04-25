b40803a550652e8044aa6996924d064d8899788d
Stephen Obialor <stevobialor@gmail.com>
2023-04-25 19:22:06 +0000

    correct syntax

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,7 @@ export default function cleanSet(set, startString) {
   if (!startString || !startString.length) return '';
 
   const array = [...set]
-    .filter((string) => {
-      if (string && string.startsWith(startString)) return string;
-    })
+    .filter((string) => string && string.startsWith(startString))
     .map((string) => string.slice(startString.length));
   return array.join('-');
 }
