2481528bc0951ac622abdc2c79ca0d8d5ac67cf7
Stephen Obialor <stevobialor@gmail.com>
2023-04-25 20:04:10 +0000

    return map

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,10 @@
 export default function groceriesList() {
-  return new map([
+  const groceries = new map([
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5]
   ]);
+  return groceries;
 }
