627a841e808deb1ea06be68aed24719b8a8b55bc
Stephen Obialor <stevobialor@gmail.com>
2023-06-10 13:32:44 +0000

    correct logic errors in code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  if (input !== '') {
-    process.stdout.write(`Your name is: ${input}\n`);
-  } else {
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input) process.stdout.write(`Your name is: ${input}`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
