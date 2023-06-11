f875a54da5d21b1047e300b8efaa9936fe02395a
Stephen Obialor <stevobialor@gmail.com>
2023-06-11 07:59:29 +0000

    correct syntax error from promise

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,17 +1,15 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  const promise = (res, rej) => {
+  const promise = (resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        rej(Error('Cannot load the database'));
-        throw new Error('Cannot load the database');
+        reject(new Error('Cannot load the database'));
+        return;
       }
-      const log = [];
       const validLines = data.split('\n');
       const students = validLines.slice(1, validLines.length - 1);
       console.log(`Number of students: ${students.length}`);
-      log.push(`Number of students: ${students.length}`);
       const fieldCount = {};
       for (const student of students) {
         const stud = student.split(',');
@@ -24,10 +22,9 @@ function countStudents(path) {
           const s = fieldCount[field].join(', ');
           const c = fieldCount[field].length;
           console.log(`Number of students in ${field}: ${c}. List: ${s}`);
-          log.push(`Number of students in ${field}: ${c}. List: ${s}`);
         }
       }
-      res(log);
+      resolve();
     });
   };
   return new Promise(promise);
