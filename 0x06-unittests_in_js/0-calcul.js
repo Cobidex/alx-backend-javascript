f68a92215a597189e81e39ed77db544f8b1a7654
Stephen Obialor <stevobialor@gmail.com>
2023-06-15 21:59:23 +0000

    make test pass

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,4 +1,5 @@
 function calculateNumber(a, b) {
+  if (isNaN(a) || isNaN(b)) throw new Error('NaN');
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
   return roundedA + roundedB;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,9 +3,16 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('should return the rounded sum of two numbers', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(-1.8, 2), 0);
+    assert.strictEqual(calculateNumber(1.8, -2), 0);
+    assert.strictEqual(calculateNumber(-1.8, -2), -4);
+    assert.strictEqual(calculateNumber(1.5, 0.4), 2);
+  });
+  it('should throw error if string passed', () => {
+    assert.throws(() => calculateNumber('st', 3), Error);
   });
 });
