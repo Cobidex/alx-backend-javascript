2722b73231595b36aad0873fa72b2147ffb90f4d
Stephen Obialor <stevobialor@gmail.com>
2023-06-15 22:22:23 +0000

    correct syntax errors in test file

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,6 +1,7 @@
 function calculateNumber(type, a, b) {
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
+  if (isNaN(a) || isNaN(b)) throw new Error('NAN');
 
   if (type === 'SUM') {
     return roundedA + roundedB;
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -7,6 +7,12 @@ describe('calculateNumber', () => {
       assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
       assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
       assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+      assert.strictEqual(calculateNumber('SUM', -1, 3.7), 3);
+      assert.strictEqual(calculateNumber('SUM', 1, -3.7), -3);
+      assert.strictEqual(calculateNumber('SUM', -1, -3.7), -5);
+    });
+    it('should throw an error when NaN is passed', () => {
+      assert.throws(() => calculateNumber('SUM', 'NaN', 3.7), Error);
     });
   });
 
@@ -14,21 +20,27 @@ describe('calculateNumber', () => {
     it('should return the rounded subtraction of two numbers', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
       assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
-      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -2);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
+      assert.strictEqual(calculateNumber('SUBTRACT', -1, 3.7), -5);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1, -3.7), 5);
+      assert.strictEqual(calculateNumber('SUBTRACT', -1, -3.7), 3);
     });
   });
 
   describe('type DIVIDE', () => {
     it('should return the rounded division of two numbers', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-      assert.strictEqual(calculateNumber('DIVIDE', 1, 3), 0.33);
+      assert.strictEqual(calculateNumber('DIVIDE', 1, 2), 0.5);
       assert.strictEqual(calculateNumber('DIVIDE', 1, 3.7), 0.25);
+      assert.strictEqual(calculateNumber('DIVIDE', -1, 3.7), -0.25);
+      assert.strictEqual(calculateNumber('DIVIDE', 1, -3.7), -0.25);
+      assert.strictEqual(calculateNumber('DIVIDE', -1, -3.7), 0.25);
     });
 
     it('should return "Error" when the rounded value of b is 0', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
       assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
-      assert.strictEqual(calculateNumber('DIVIDE', 1.7, 0), 'Error');
+      assert.strictEqual(calculateNumber('DIVIDE', -1.7, 0), 'Error');
     });
   });
 });
